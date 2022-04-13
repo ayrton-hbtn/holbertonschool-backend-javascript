149248ddd5054a4fa4a4cef106cacb8b9064c122
ayrton-hbtn <3282@holbertonschool.com>
2022-04-13 17:10:33 -0300

    return

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   const photo = uploadPhoto();
   const user = createUser();
 
-  Promise.all([photo, user]).then((values) => {
+  return Promise.all([photo, user]).then((values) => {
     console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
   })
     .catch(() => { console.log('Signup system offline'); });
