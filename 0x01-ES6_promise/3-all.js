84a2aa9077a9236f16a2843a78a3597fa848da7b
ayrton-hbtn <3282@holbertonschool.com>
2022-04-13 17:05:32 -0300

    fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto().then((data) => data.body).then((data) => {
-    createUser().then((res) => {
-      console.log(`${data} ${res.firstName} ${res.lastName}`);
-    });
+  const photo = uploadPhoto();
+  const user = createUser();
+
+  Promise.all([photo, user]).then((values) => {
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
   })
     .catch(() => { console.log('Signup system offline'); });
 }
