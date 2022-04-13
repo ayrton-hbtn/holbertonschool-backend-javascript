57060468ab49e79f1607709b177d6e9c7b27ac66
ayrton-hbtn <3282@holbertonschool.com>
2022-04-13 20:54:18 -0300

    eslint fix

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,16 +1,15 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-    const userUpload = {};
+  const userUpload = {};
 
-    try {
-        userUpload.photo = await uploadPhoto();
-        userUpload.user = await createUser();
-    }
-    catch {
-        userUpload.photo = null;
-        userUpload.user = null;
-    }
+  try {
+    userUpload.photo = await uploadPhoto();
+    userUpload.user = await createUser();
+  } catch (err) {
+    userUpload.photo = null;
+    userUpload.user = null;
+  }
 
-    return userUpload;
+  return userUpload;
 }
