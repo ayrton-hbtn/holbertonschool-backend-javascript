4176ca9782a5b7968cf852ac34e5487ce8380509
ayrton-hbtn <3282@holbertonschool.com>
2022-04-19 11:04:34 -0300

    more corner cases

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,27 +1,31 @@
+/* eslint-disable no-continue */
 export default function cleanSet(set, startString) {
   if (!(set instanceof Set)) {
     throw new TypeError('set must be of type Set');
   }
-
-  if (typeof (startString) !== 'string') {
-    throw new TypeError('startString must be of type String');
-  }
-
   let returnStr = '';
-
   if (startString.length === 0) {
     return returnStr;
   }
-
+  if (typeof (startString) !== 'string') {
+    throw new TypeError('startString must be of type String');
+  }
   for (const str of set) {
     if (typeof (str) !== 'string') {
       throw new TypeError('set must contain strings only');
     }
-
     if (str.startsWith(startString)) {
+      if ((str.slice(startString.length)).length === 0) {
+        continue;
+      }
       returnStr += `${str.slice(startString.length)}-`;
     }
   }
-
-  return returnStr.slice(0, returnStr.length - 1);
+  if (returnStr.charAt(0) === '-') {
+    returnStr = returnStr.slice(1);
+  }
+  if (returnStr.charAt(returnStr.length - 1) === '-') {
+    returnStr = returnStr.slice(0, returnStr.length - 1);
+  }
+  return returnStr;
 }
