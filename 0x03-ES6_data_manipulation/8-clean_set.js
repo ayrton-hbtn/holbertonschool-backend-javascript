d76bf6127a1c30bc82d519ff5802af946ffba545
ayrton-hbtn <3282@holbertonschool.com>
2022-04-19 10:41:07 -0300

    fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,20 +2,26 @@ export default function cleanSet(set, startString) {
   if (!(set instanceof Set)) {
     throw new TypeError('set must be of type Set');
   }
-  let returnStr = '';
-  if (startString.length === 0) {
-    return returnStr;
-  }
+
   if (typeof (startString) !== 'string') {
     throw new TypeError('startString must be of type String');
   }
+
+  let returnStr = '';
+
+  if (startString.length === 0) {
+    return returnStr;
+  }
+
   for (const str of set) {
     if (typeof (str) !== 'string') {
       throw new TypeError('set must contain strings only');
     }
+
     if (str.startsWith(startString)) {
       returnStr += `${str.slice(startString.length)}-`;
     }
   }
+
   return returnStr.slice(0, returnStr.length - 1);
 }
