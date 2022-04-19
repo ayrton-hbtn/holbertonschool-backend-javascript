d7e7edde74ca9f155a9366f9caaf6883bc10c6d7
ayrton-hbtn <3282@holbertonschool.com>
2022-04-19 11:08:54 -0300

    fix

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,9 +4,8 @@ export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 1);
   } else {
-    const queries = weakMap.get(endpoint);
-    weakMap.set(endpoint, queries + 1);
-    if (queries >= 5) {
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+    if (weakMap.get(endpoint) >= 5) {
       throw new Error('Endpoint load is high');
     }
   }
