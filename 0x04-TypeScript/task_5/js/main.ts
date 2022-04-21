fb85e86aa88ad648f41525ab3fae5d6f52262d04
ayrton-hbtn <3282@holbertonschool.com>
2022-04-20 23:05:13 -0300

    fix

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -9,9 +9,9 @@ interface MinorCredits {
 }
 
 function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): number {
-    return (subject1.credits + subject1.credits);
+    return (subject1.credits + subject2.credits);
 }
 
 function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): number {
-    return (subject1.credits + subject1.credits);
+    return (subject1.credits + subject2.credits);
 }
