ffa744355db3bd961735dded2110d6abe742bf82
ayrton-hbtn <3282@holbertonschool.com>
2022-06-07 23:19:57 -0300

    eslint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,25 +1,27 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function countStudents(filepath) {
-    let data = new Array;
-    let fields = new Object;
-    try {
-        data = fs.readFileSync(filepath).toString().split('\n').slice(1);
+  let data = [];
+  const fields = {};
+  try {
+    data = fs.readFileSync(filepath).toString().split('\n').slice(1);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+  console.log('Number of students: ' + data.length);
+  data.map((student) => student.split(','))
+      .map((student) => {
+        if (!(student[3] in fields)) {
+          fields[student[3]] = [];
+        }
+        fields[student[3]].push(student[0]);
+      });
+  for (const key in fields) {
+    if (Object.prototype.hasOwnProperty.call(fields, key)) {
+      console.log((`Number of students in ${key}: ${fields[key].length}.\
+        List: ${fields[key]}`).replace(/,/g, ', '));
     }
-    catch (err) {
-        throw new Error('Cannot load the database');
-    }
-    console.log("Number of students: " + data.length);
-    data.map(student => student.split(','))
-        .map(student => {
-            if (!(student[3] in fields)) {
-                fields[student[3]] = new Array;
-            }
-            fields[student[3]].push(student[0])
-        });
-    for (const key in fields) {
-        console.log((`Number of students in ${key}: ${fields[key].length}. List: ${fields[key]}`).replace(/,/g, ', '));
-    }
+  }
 }
 
 module.exports = countStudents;
