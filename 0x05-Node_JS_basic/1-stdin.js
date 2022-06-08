380c6b4f9e48ffdd636bde1964887cb4ff688290
ayrton-hbtn <3282@holbertonschool.com>
2022-06-07 23:15:39 -0300

    fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,10 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-rl.on('line', (line) => {
-  process.stdout.write('Your name is: ' + line + '\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
 });
 
-rl.on('SIGINT', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-  rl.close();
 });
